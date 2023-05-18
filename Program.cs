86f53eaec45fe0744c7b833d1f20b47503e163fc
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-18 13:36:53 -0700

    new

--- Program.cs
+++ Program.cs
@@ -3,14 +3,18 @@
     static void Main(string[] args)
     {
         //can only use the ref keyword if your variable has an initial value
-      int studentGrade = 75;
+      //int studentGrade = 75;
+      //to use with no value needs the out keyword
+      int studentGrade;
         //passes location of memory by reference and not by memory
-        ExtraCreditRef(ref studentGrade);
+        ExtraCreditRef(out studentGrade);
         Console.WriteLine(studentGrade);
     //for this example it would be the same with or without the ref keyword
     }
-    static void ExtraCreditRef( ref int studentGrade)
+    static void ExtraCreditRef( out int studentGrade)
     {
+        //have to give the original  value
+            studentGrade = 0;
             studentGrade +=3;
 
     }
